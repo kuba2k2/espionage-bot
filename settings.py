8c3a2e83ad9cae9fa1f17dc276adbb07aa58ac8f
Kuba Szczodrzyński <kuba@szczodrzynski.pl>
2021-03-10 15:15:55 +0100

    Do a full rewrite because the author didn't read the docs

--- settings.py
+++ settings.py
@@ -6,5 +6,5 @@ load_dotenv(encoding="utf-8")
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 ESPIONAGE_FILE = os.getenv("ESPIONAGE_FILE")
-COMMANDS = os.getenv("COMMANDS").split(" ")
-FILES = os.getenv("FILES").split(" ")
+FILES_JSON = os.getenv("FILES_JSON")
+ACTIVITY_NAME = os.getenv("ACTIVITY_NAME")
--- start.py
+++ start.py
@@ -1,161 +1,191 @@
-from asyncio import sleep
-from typing import List, Dict
+import json
+from typing import Dict
 
-import discord
 from discord import (
+    Activity,
+    ActivityType,
+    Guild,
+    User,
+    Member,
     VoiceChannel,
     VoiceClient,
-    FFmpegOpusAudio,
-    User,
-    Message,
-    Guild,
-    Member,
     VoiceState,
+    FFmpegOpusAudio,
 )
+from discord.ext import commands
+from discord.ext.commands import Cog, Context, Bot, CommandError
 
-from settings import BOT_TOKEN, ESPIONAGE_FILE, COMMANDS, FILES
+from settings import BOT_TOKEN, ESPIONAGE_FILE, FILES_JSON, ACTIVITY_NAME
 
-client = discord.Client()
+client = Bot(command_prefix=commands.when_mentioned_or("!"))
 
-voice_clients: Dict[int, VoiceClient] = {}
-voice_channels: Dict[int, VoiceChannel] = {}
-voice_files: Dict[int, str] = {}
+
+class FFmpegFileOpusAudio(FFmpegOpusAudio):
+    def __init__(self, filename: str, *args, **kwargs):
+        self.filename = filename
+        super().__init__(filename, *args, **kwargs)
+
+
+async def connect_to(channel: VoiceChannel) -> VoiceClient:
+    guild: Guild = channel.guild
+    voice: VoiceClient = guild.voice_client
+    if voice is None or not voice.is_connected():
+        voice = await channel.connect()
+    elif voice.channel != channel:
+        await voice.move_to(channel)
+    elif voice.is_playing():
+        voice.pause()
+    return voice
+
+
+def is_alone(voice: VoiceClient):
+    if not voice:
+        return False
+    return len(voice.channel.voice_states) <= 1
+
+
+async def disconnect(voice: VoiceClient):
+    await voice.disconnect()
+
+
+async def ensure_voice(_, ctx: Context):
+    member: Member = ctx.args[2] or ctx.author
+    if not member.voice:
+        await ctx.send(f"User is not connected to a voice channel.", delete_after=3)
+        raise CommandError(f"{ctx.author} not connected to a voice channel.")
+    await connect_to(member.voice.channel)
+
+
+class Espionage(Cog, name="Music commands"):
+    def __init__(self, bot: Bot, files: Dict[str, str]):
+        self.bot = bot
+        self.files = files
+        self.bot.event(self.on_voice_state_update)
+        for name in files.keys():
+            cmd = self.files[name]
+            command = self.bot.command(
+                name=name,
+                brief=cmd["help"] or f"Play {cmd['filename']}",
+            )(self.play_command)
+            command.before_invoke(ensure_voice)
+            command.cog = self
+
+    async def play_command(self, _, ctx: Context, __: User = None):
+        cmd = ctx.command.name
+        cmd = self.files[cmd]
+        # force playing the specified file
+        await self.play(
+            ctx.voice_client.channel,
+            file=cmd["filename"],
+            loop=cmd["loop"],
+        )
+
+    async def on_voice_state_update(
+        self, member: Member, before: VoiceState, after: VoiceState
+    ):
+        if member.id == self.bot.user.id:
+            # the bot was moved to (or joined) the AFK channel
+            if after.mute or after.afk:
+                await member.edit(mute=False)
+            return
+
+        # the bot is the only connected user
+        if is_alone(member.guild.voice_client):
+            await disconnect(member.guild.voice_client)
+
+        # the channel is unchanged
+        if before.channel == after.channel:
+            return
+
+        # a user left a voice channel
+        if not after.channel:
+            return
+
+        # a user joined the AFK channel
+        if (not before.afk or not before.channel) and after.afk:
+            # play the default file or leave the currently playing file
+            await self.play(
+                member.voice.channel,
+                file=ESPIONAGE_FILE if not member.guild.voice_client else None,
+                loop=True,
+            )
+            return
+
+        # a user joined the channel when the bot was alone
+        # for some reason this fixes silence when moving the bot
+        # to an empty channel
+        if (
+            member.guild.voice_client
+            and member.guild.voice_client.channel == after.channel
+            and len(after.channel.voice_states) == 2
+        ):
+            await self.play(
+                after.channel,
+                file=None,
+                loop=True,
+            )
+
+    async def play(self, channel: VoiceChannel, file: str, loop: bool):
+        # connect to the specified voice channel
+        await connect_to(channel)
+        # repeat the file
+        self.repeat(channel, file, loop)
+
+    def repeat(
+        self,
+        channel: VoiceChannel,
+        file: str = None,
+        loop: bool = True,
+        repeated: bool = False,
+    ):
+        # get the currently connected voice client
+        voice: VoiceClient = channel.guild.voice_client
+
+        # the bot is no longer connected
+        if not voice:
+            return
+
+        # the current source differs from the desired source
+        if voice.source and file and voice.source.filename != file:
+            # when changing the playing file avoid repeating the previous one
+            if repeated:
+                return
+            if voice.is_playing() or voice.is_paused():
+                voice.stop()
+            else:
+                return
+        # return if already playing
+        if voice.is_playing():
+            return
+        # resume if audio paused
+        if voice.is_paused():
+            voice.resume()
+            return
+
+        # file not specified - not to change the already playing file
+        if not file:
+            return
+        source = FFmpegFileOpusAudio(file)
+        voice.play(
+            source,
+            after=lambda e: not loop
+            or self.repeat(channel, file=file, loop=loop, repeated=True),
+        )
 
 
 @client.event
 async def on_ready():
     print("We have logged in as {0.user}".format(client))
-
-
-@client.event
-async def on_voice_state_update(member: Member, before: VoiceState, after: VoiceState):
-    if member.id != client.user.id and (
-        not after.afk or (before.channel and before.afk == after.afk)
-    ):
-        return
-    if before.channel == after.channel:
-        return
-    guild: Guild = member.guild
-    if member.id == client.user.id and not after.channel:
-        voice_clients.pop(guild.id, None)
-        voice_channels.pop(guild.id, None)
-    if member.id == client.user.id and (not before.channel or not after.channel):
-        return
-
-    channel: VoiceChannel = after.channel
-
-    # return if the bot is already connected
-    if (
-        guild.id in voice_channels
-        and voice_clients[guild.id].is_connected()
-        and voice_channels[guild.id].id == after.channel.id
-    ):
-        return
-
-    # a user joined the afk channel, reset the file
-    if member.id != client.user.id and after.afk and before.afk != after.afk:
-        voice_files.pop(guild.id, None)
-
-    file = voice_files[guild.id] if guild.id in voice_files else ESPIONAGE_FILE
-    await play(channel, file)
-
-
-async def play(channel: VoiceChannel, file: str):
-    guild: Guild = channel.guild
-
-    if guild.id in voice_clients:
-        voice: VoiceClient = voice_clients[guild.id]
-        if channel.id != voice.channel.id:
-            print(f"+ Moving from {voice.channel} to {channel}")
-            await voice.move_to(channel)
-        if voice.is_paused():
-            print(f"? Paused on {voice.channel}")
-            await play_file(voice, file)
-            return
-    else:
-        print(f"+ Joining {channel}")
-        voice: VoiceClient = await channel.connect()
-    voice_clients[guild.id] = voice
-    voice_channels[guild.id] = channel
-
-    members: List[Member] = await guild.query_members(user_ids=[client.user.id])
-    if not members:
-        return
-    member: Member = members[0]
-    # unmute self on AFK channel
-    if member.voice.mute or member.voice.afk:
-        print(f"? Unmuting self on {voice.channel}")
-        await member.edit(mute=False)
-
-    await play_file(voice, file)
-
-
-async def play_file(voice: VoiceClient, file: str):
-    voice.resume()
-    # not connected - disconnect and play again
-    if not voice.is_connected():
-        print(f"!! Not connected to {voice.channel}, reconnecting...")
-        await disconnect(voice)
-        await play(voice.channel, file)
-        return
-
-    guild_id: int = voice.guild.id
-
-    current_file = voice_files[guild_id] if guild_id in voice_files else None
-    voice_files[guild_id] = file
-
-    while voice.is_connected():
-        file = voice_files[guild_id]
-        print(f"> Playing {file} on {voice.channel}")
-        if voice.is_paused() and file == current_file:
-            voice.resume()
-        elif not voice.is_playing() or file != current_file:
-            voice.stop()
-            source = FFmpegOpusAudio(file)
-            voice.play(source)
-        while voice.is_playing() and voice.is_connected():
-            await sleep(0.1)
-        # voice.stop()
-        if len(voice.channel.voice_states) == 1 and voice.is_connected():
-            await disconnect(voice)
-    # voice_clients.pop(guild.id, None)
-    # voice_channels.pop(guild.id, None)
-
-
-async def disconnect(voice: VoiceClient):
-    print(f"- Disconnecting from {voice.channel}")
-    await voice.disconnect(force=True)
-    guild: Guild = voice.guild
-    voice_clients.pop(guild.id, None)
-    voice_channels.pop(guild.id, None)
-
-
-@client.event
-async def on_message(message: Message):
-    if message.author == client.user:
-        return
-    args: List[str] = message.content.split(" ")
-    if not args or args[0] not in COMMANDS:
-        return
-
-    index = COMMANDS.index(args[0])
-    file = FILES[index]
-
-    author: User = message.author
-    guild: Guild = message.guild
-    members: List[Member] = await guild.query_members(user_ids=[author.id])
-    if not members:
-        return
-    member: Member = members[0]
-    state: VoiceState = member.voice
-    if not state:
-        return
-    channel: VoiceChannel = state.channel
-    if not channel:
-        return
-
-    await play(channel, file)
+    await client.change_presence(
+        activity=Activity(
+            type=ActivityType.listening,
+            name=ACTIVITY_NAME,
+        )
+    )
 
 
 if __name__ == "__main__":
+    with open(FILES_JSON, "r") as f:
+        files_json = json.load(f)
+    client.add_cog(Espionage(bot=client, files=files_json))
     client.run(BOT_TOKEN)
