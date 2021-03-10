38bc8a5169f2b6917cea41b5ac162f380badc389
Kuba Szczodrzyński <kuba@szczodrzynski.pl>
2021-03-10 16:51:07 +0100

    Add uploading files and changing loop mode

--- start.py
+++ start.py
@@ -1,4 +1,7 @@
 import json
+from os import unlink
+from os.path import isfile
+from time import time
 from typing import Dict
 
 from discord import (
@@ -11,11 +14,13 @@ from discord import (
     VoiceClient,
     VoiceState,
     FFmpegOpusAudio,
+    Message,
+    Attachment,
 )
 from discord.ext import commands
-from discord.ext.commands import Cog, Context, Bot, CommandError
+from discord.ext.commands import Cog, Context, Bot, CommandError, Command
 
-from settings import BOT_TOKEN, ESPIONAGE_FILE, FILES_JSON, ACTIVITY_NAME
+from settings import BOT_TOKEN, ESPIONAGE_FILE, FILES_JSON, ACTIVITY_NAME, UPLOAD_PATH
 
 client = Bot(command_prefix=commands.when_mentioned_or("!"))
 
@@ -57,19 +62,33 @@ async def ensure_voice(_, ctx: Context):
 
 
 class Espionage(Cog, name="Music commands"):
-    def __init__(self, bot: Bot, files: Dict[str, str]):
+    def __init__(self, bot: Bot, files: Dict[str, dict]):
         self.bot = bot
         self.files = files
         self.bot.event(self.on_voice_state_update)
         for name in files.keys():
-            cmd = self.files[name]
-            command = self.bot.command(
-                name=name,
-                brief=cmd["help"] or f"Play {cmd['filename']}",
-            )(self.play_command)
-            command.before_invoke(ensure_voice)
-            command.cog = self
+            self.add_command(name)
 
+    def add_command(self, name: str):
+        if self.bot.get_command(name):
+            return
+        cmd = self.files[name]
+        command: Command = self.bot.command(
+            name=name,
+            brief=cmd["help"]
+            or (
+                f"Loop {cmd['filename']}" if cmd["loop"] else f"Play {cmd['filename']}"
+            ),
+        )(self.play_command)
+        command.before_invoke(ensure_voice)
+        command.cog = self
+
+    def remove_command(self, name: str):
+        if not self.bot.get_command(name):
+            return
+        self.bot.remove_command(name)
+
+    @commands.guild_only()
     async def play_command(self, _, ctx: Context, __: User = None):
         cmd = ctx.command.name
         cmd = self.files[cmd]
@@ -173,6 +192,157 @@ class Espionage(Cog, name="Music commands"):
         )
 
 
+class Uploading(Cog, name="File uploading/management"):
+    def __init__(self, bot: Bot, path: str):
+        self.bot = bot
+        self.path = path
+
+    @commands.command()
+    @commands.guild_only()
+    async def upload(self, ctx: Context, name: str = None):
+        """Upload the attached file as a command."""
+        message: Message = ctx.message
+        if not name:
+            await ctx.send(
+                "Usage: `!upload <command name>`. Attach one audio file.",
+                delete_after=3,
+            )
+            return
+        if len(message.attachments) != 1:
+            await ctx.send("You must add exactly one attachment.", delete_after=3)
+            return
+        attachment: Attachment = message.attachments[0]
+
+        cog: Espionage = self.bot.get_cog("Music commands")
+        if not cog:
+            return
+        files = cog.files
+
+        replaced = False
+        # delete the already existing file
+        if self.bot.get_command(name):
+            replaced = True
+            unlink(files[name]["filename"])
+
+        # create a safe filename
+        filename = f"{self.path}/{int(time())}_{attachment.filename}"
+
+        # save the attachment
+        with open(filename, "wb") as f:
+            await attachment.save(f)
+
+        # replace the command descriptor
+        files[name] = {
+            "filename": filename,
+            "help": f"Uploaded by {ctx.author}",
+            "loop": True,
+        }
+
+        # add the command to the music cog
+        cog.add_command(name)
+        # save the command descriptors
+        with open(FILES_JSON, "w") as f:
+            json.dump(files, f, indent=4)
+
+        await ctx.send(
+            f"File **{attachment.filename}** uploaded as `!{name}`."
+            if not replaced
+            else f"Replaced `!{name}` with **{attachment.filename}**.",
+            delete_after=10,
+        )
+
+    @commands.command()
+    @commands.guild_only()
+    async def eloop(self, ctx: Context, name: str = None):
+        """Enable looping of the specified audio."""
+        if not name:
+            await ctx.send("Usage: `!enoloop <command name>`.", delete_after=3)
+            return
+        if not self.set_loop(name, True):
+            await ctx.send(f"The command `!{name}` does not exist.", delete_after=3)
+            return
+        await ctx.send(f"Looping enabled for `!{name}`.", delete_after=3)
+
+    @commands.command()
+    @commands.guild_only()
+    async def enoloop(self, ctx: Context, name: str = None):
+        """Disable looping of the specified audio."""
+        if not name:
+            await ctx.send("Usage: `!enoloop <command name>`.", delete_after=3)
+            return
+        if not self.set_loop(name, False):
+            await ctx.send(f"The command `!{name}` does not exist.", delete_after=3)
+            return
+        await ctx.send(f"Looping disabled for `!{name}`.", delete_after=3)
+
+    def set_loop(self, name: str, loop: bool) -> bool:
+        cog: Espionage = self.bot.get_cog("Music commands")
+        if not cog:
+            return
+        files = cog.files
+
+        if name not in files:
+            return False
+
+        if files[name]["loop"] != loop:
+            files[name]["loop"] = loop
+            # remove the command to update help text
+            cog.remove_command(name)
+            cog.add_command(name)
+            # save the command descriptors
+            with open(FILES_JSON, "w") as f:
+                json.dump(files, f, indent=4)
+        return True
+
+    @commands.command()
+    @commands.guild_only()
+    async def aremove(self, ctx: Context, name: str = None):
+        """Remove the specified audio."""
+        if not name:
+            await ctx.send("Usage: `!aremove <command name>`.", delete_after=3)
+            return
+        cog: Espionage = self.bot.get_cog("Music commands")
+        if not cog:
+            return
+        files = cog.files
+        if name not in files:
+            await ctx.send(f"The command `!{name}` does not exist.", delete_after=3)
+            return
+        cog.remove_command(name)
+        cmd = files.pop(name, None)
+        if cmd and isfile(cmd["filename"]):
+            unlink(cmd["filename"])
+        await ctx.send(f"Command `!{name}` removed.", delete_after=3)
+
+    @commands.command()
+    @commands.guild_only()
+    async def description(self, ctx: Context, name: str = None, *args):
+        """Change the description."""
+        description = " ".join(args)
+        if not name or not description:
+            await ctx.send(
+                "Usage: `!description <command name> <description>`.", delete_after=3
+            )
+            return
+        cog: Espionage = self.bot.get_cog("Music commands")
+        if not cog:
+            return
+        files = cog.files
+        if name not in files:
+            await ctx.send(f"The command `!{name}` does not exist.", delete_after=3)
+            return
+        files[name]["help"] = description
+        # remove the command to update help text
+        cog.remove_command(name)
+        cog.add_command(name)
+        # save the command descriptors
+        with open(FILES_JSON, "w") as f:
+            json.dump(files, f, indent=4)
+        await ctx.send(
+            f"Description of `!{name}` set to `{description}`.", delete_after=3
+        )
+
+
 @client.event
 async def on_ready():
     print("We have logged in as {0.user}".format(client))
@@ -185,7 +355,8 @@ async def on_ready():
 
 
 if __name__ == "__main__":
-    with open(FILES_JSON, "r") as f:
-        files_json = json.load(f)
+    with open(FILES_JSON, "r") as jf:
+        files_json = json.load(jf)
     client.add_cog(Espionage(bot=client, files=files_json))
+    client.add_cog(Uploading(bot=client, path=UPLOAD_PATH))
     client.run(BOT_TOKEN)
